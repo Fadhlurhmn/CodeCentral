a92c78c34f25ebd55d7c7b708ca0459d9685e123
Fadhlurhmn <fadhlu.campus22@gmail.com>
2024-05-02 10:42:36 +0700

    penambahan controller dan model untuk promosi

---
+++ app/Http/Controllers/PromosiController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\KeluargaModel;
+use App\Models\PromosiModel;
+use Yajra\DataTables\Facades\DataTables;
+
+class PromosiController extends Controller
+{
+    public function index()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Daftar Promosi',
+            'list' => ['Home', 'Promosi']
+        ];
+
+        $page = (object)[
+            'title' => 'Daftar promosi yang terdaftar'
+        ];
+
+        $activeMenu = 'promosi';
+
+        $keluarga = KeluargaModel::all();
+
+        return view('promosi', ['breadcrumb' => $breadcrumb, 'page' => $page, 'keluarga' => $keluarga, 'activeMenu' => $activeMenu]);
+    }
+    public function list(Request $request)
+    {
+        $promosi = PromosiModel::select('id_umkm', 'nama_usaha', 'gambar', 'deskripsi', 'status_pengajuan', 'alamat', 'countdown', 'id_keluarga')
+            ->with('keluarga');
+
+        if ($request->keluarga_id) {
+            $promosi->where('id_keluarga', $request->keluarga_id);
+        }
+
+        // Filter berdasarkan usaha per keluarga 
+        if ($request->has('id_keluarga')) {
+            $promosi->where('id_keluarga', $request->id_keluarga);
+        }
+        return DataTables::of($promosi)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($promosi) {
+                $btn = '<a href="' . url('/promosi/' . $promosi->id_umkm . '/show') . '" class="btn btn-primary ml-1 flex-col "><i class="fas fa-info-circle"></i></i></a> ';
+                $btn .= '<a href="' . url('/promosi/' . $promosi->id_umkm . '/edit') . '" class="btn btn-info ml-2 mr-2 flex-col"><i class="fas fa-edit"></i></a> ';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    public function create()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Tambah Promosi',
+            'list' => ['Home', 'Promosi', 'Tambah']
+        ];
+
+        $page = (object)[
+            'title' => 'Tambah promosi baru'
+        ];
+
+        $keluarga = KeluargaModel::all(); // ambil data keluar$keluarga untuk ditampilkan di form
+        $activeMenu = 'promosi'; // set menu yang sedang aktif
+
+        return view('admin.promosi.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'keluarga' => $keluarga, 'activeMenu' => $activeMenu]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_usaha' => 'required|string',
+            'gambar' => 'required|string',
+            'deskripsi' => 'required|string',
+            'status_pengajuan' => 'required|string',
+            'alamat' => 'required|string',
+            'countdown' => 'required|string',
+            'id_keluarga' => 'required|integer',
+        ]);
+
+        PromosiModel::create([
+            'nama_usaha' => $request->nama_usaha,
+            'gambar' => $request->gambar,
+            'deskripsi' => $request->deskripsi,
+            'status_pengajuan' => $request->status_pengajuan,
+            'alamat' => $request->alamat,
+            'countdown' => $request->countdown,
+            'id_keluarga' => $request->id_keluarga,
+        ]);
+
+        return redirect('/promosi')->with('success', 'Data promosi berhasil disimpan');
+    }
+    public function show(string $id)
+    {
+        $promosi = PromosiModel::with('keluarga')->find($id);
+        $nomor_kk = $promosi->keluarga->nomor_keluarga;
+
+        $breadcrumb = (object)[
+            'title' => 'Detail Promosi',
+            'list' => ['Home', 'Promosi', 'Detail']
+        ];
+
+        $page = (object)[
+            'title' => 'Detail data promosi '
+        ];
+
+        $activeMenu = 'promosi'; // set menu yang sedang aktif
+
+        return view('admin.promosi.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'promosi' => $promosi, 'activeMenu' => $activeMenu]);
+    }
+    public function edit(string $id)
+    {
+        $promosi = PromosiModel::find($id);
+        $keluarga = KeluargaModel::all();
+
+        $breadcrumb = (object) [
+            'title' => 'Edit Promosi',
+            'list' => ['Home', 'Promosi', 'Edit']
+        ];
+
+        $page = (object) [
+            'title' => 'Ubah data promosi'
+        ];
+
+        $activeMenu = 'promosi'; // set menu yang sedang aktif
+
+        return view('admin.promosi.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'promosi' => $promosi, 'keluarga' => $keluarga, 'activeMenu' => $activeMenu]);
+    }
+    // menyimpan perubahan data barang
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nama_usaha' => 'required|string',
+            'gambar' => 'required|string',
+            'deskripsi' => 'required|string',
+            'status_pengajuan' => 'required|string',
+            'alamat' => 'required|string',
+            'countdown' => 'required|string',
+            'id_keluarga' => 'required|integer',
+        ]);
+
+        PromosiModel::find($id)->update([
+            'nama_usaha' => $request->nama_usaha,
+            'gambar' => $request->gambar,
+            'deskripsi' => $request->deskripsi,
+            'status_pengajuan' => $request->status_pengajuan,
+            'alamat' => $request->alamat,
+            'countdown' => $request->countdown,
+            'id_keluarga' => $request->id_keluarga,
+        ]);
+
+        return redirect('/promosi/' . $id . '/show')->with('success', 'Data promosi berhasil diubah');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PromosiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,12 +9,24 @@ Route::get('/', function () {
 
 // Template 
 Route::group(['prefix' => 'template'], function () {
-    Route::get('/', function () {return view('template.index');});
-    Route::get('/shop', function () {return view('template.index-1');});
-    Route::get('/email', function () {return view('template.email');});
-    Route::get('/typography', function () {return view('template.typography');});
-    Route::get('/alert', function () {return view('template.alert');});
-    Route::get('/buttons', function () {return view('template.buttons');});
+    Route::get('/', function () {
+        return view('template.index');
+    });
+    Route::get('/shop', function () {
+        return view('template.index-1');
+    });
+    Route::get('/email', function () {
+        return view('template.email');
+    });
+    Route::get('/typography', function () {
+        return view('template.typography');
+    });
+    Route::get('/alert', function () {
+        return view('template.alert');
+    });
+    Route::get('/buttons', function () {
+        return view('template.buttons');
+    });
 });
 
 // Admin, RT & RW Login 
@@ -30,17 +43,48 @@ Route::get('/logout', function () {
 
 // Route for admin (for template only, you can make your own controller with index inside and remove this)
 Route::group(['prefix' => 'admin'], function () {
-    Route::get('/', function () {return view('admin.index');});
-    Route::get('/akun', function () {return view('admin.akun.index');});
-    Route::get('/bansos', function () {return view('admin.bansos.index');});
-    Route::get('/penduduk', function () {return view('admin.penduduk.index');});
-    Route::get('/pengumuman', function () {return view('admin.pengumuman.index');});
-    Route::get('/promosi', function () {return view('admin.promosi.index');});
-    Route::get('/promosi/daftar', function () {return view('admin.promosi.daftar');}); //untuk halaman daftar dari promosi
-    Route::get('/promosi/show', function () {return view('admin.promosi.show');}); //untuk halaman daftar dari promosi
-    Route::get('/surat', function () {return view('admin.surat.index');});
-    Route::get('/jadwal', function () {return view('admin.jadwal.index');});
-    Route::get('/keuangan', function () {return view('admin.keuangan.index');});
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+    Route::get('/akun', function () {
+        return view('admin.akun.index');
+    });
+    Route::get('/bansos', function () {
+        return view('admin.bansos.index');
+    });
+    Route::get('/penduduk', function () {
+        return view('admin.penduduk.index');
+    });
+    Route::get('/pengumuman', function () {
+        return view('admin.pengumuman.index');
+    });
+    Route::get('/promosi', function () {
+        return view('admin.promosi.index');
+    });
+    Route::get('/promosi/daftar', function () {
+        return view('admin.promosi.daftar');
+    }); //untuk halaman daftar dari promosi
+    Route::get('/promosi/show', function () {
+        return view('admin.promosi.show');
+    }); //untuk halaman daftar dari promosi
+    Route::get('/surat', function () {
+        return view('admin.surat.index');
+    });
+    Route::get('/jadwal', function () {
+        return view('admin.jadwal.index');
+    });
+    Route::get('/keuangan', function () {
+        return view('admin.keuangan.index');
+    });
 });
 
-
+Route::group(['prefix' => 'promosi'], function () {
+    Route::get('/', [PromosiController::class, 'index']);          // menampilkan halaman awal level
+    Route::post('/list', [PromosiController::class, 'list']);      //menampilkan data level dalam bentuk json untuk datatables
+    Route::get('/create', [PromosiController::class, 'create']);   // menampilkan halaman form tambah level
+    Route::post('/', [PromosiController::class, 'store']);         // menyimpan data level baru
+    Route::get('/{id}/show', [PromosiController::class, 'show']);       // menampilkan detail level
+    Route::get('/{id}/edit', [PromosiController::class, 'edit']);  // menampilkan halaman form edit level
+    Route::post('/{id}', [PromosiController::class, 'update']);     // menyimpan perubahan data level
+    Route::delete('/{id}', [PromosiController::class, 'destroy']); // menghapus data level
+});
