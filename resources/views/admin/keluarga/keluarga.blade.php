b3a20c177b8c9eea11f47482f56665ba9c37224b
Alerakha <capupucino@gmail.com>
2024-05-03 08:10:12 +0700

    perbaikan front end keluarga

--- resources/views/admin/keluarga/keluarga.blade.php
+++ resources/views/admin/keluarga/keluarga.blade.php
@@ -10,7 +10,7 @@
   
     
     <!-- strat content -->
-    <div class="flex-col" style="width: 1147px"> 
+    <div class="flex-col flex-grow"> 
       @include('admin.keluarga.index')
     </div>
   <!-- end content -->
