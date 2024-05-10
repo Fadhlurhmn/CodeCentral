c267e7f6b7e2924831cc3848e11ffed4508d7d60
Alerakha <capupucino@gmail.com>
2024-05-10 16:07:37 +0700

    Front End fitur promosi (admin)

--- resources/views/admin/promosi/daftar.blade.php
+++ resources/views/admin/promosi/daftar.blade.php
@@ -13,213 +13,41 @@
   <div class="bg-gray-100 flex-1 p-6 md:mt-16"> 
     <h1 class="py-5 ml-5 text-3xl text-gray-900 font-bold">{{$breadcrumb->title}}</h1>
 
-    <div class="container p-5 mb-5 bg-teal-600 text-lg grid grid-cols-2 items-center">
+    <div class="container p-5 mb-5 bg-teal-600 text-lg flex flex-col items-start">
         <p class="text-white text-xl">{{$page->title}}</p>
-        <br>
-        <a href="{{ url('admin/promosi/') }}" class=" mt-2 px-2 py-1 bg-teal-400 text-teal-900 text-center text-sm font-medium rounded-full justify-self-start">Kembali</a>
+        <a href="{{ url('admin/promosi/') }}" class=" mt-2 px-2 py-1 mb-5 bg-teal-400 text-teal-900 text-center text-sm font-medium rounded-full justify-self-start">Kembali</a>
+        {{-- Search form --}}
+        <form action="{{ url()->current() }}" method="GET" class="text-sm font-medium items-end">
+          <input type="text" name="query" value="{{ request('query') }}" placeholder="Cari nama usaha..." class="px-4 py-2 border border-gray-300 rounded-md">
+          <button type="submit" class="px-4 py-2 bg-teal-400 text-teal-900 rounded-md ml-2">Cari</button>
+        </form>
     </div>
-    
-  
-    {{-- Search form --}}
-     <form action="" method="GET" class="mb-4">
-      <input type="text" name="query" placeholder="Cari nama usaha..." class="px-4 py-2 border border-gray-300 rounded-md">
-      <button type="submit" class="px-4 py-2 bg-teal-400 text-teal-900 rounded-md ml-2">Cari</button>
-    </form>
-    {{-- Place your code here --}}
-    <div class="container bg-white text-lg grid grid-cols-6 gap-5 p-5 mx-auto overflow-y-auto" style="max-height: 80vh;">
-        {{-- Bagian Promosi Usaha Warga --}}
-        {{-- Usaha warga dari database --}}
+
+    {{-- Bagian Daftar Promosi Usaha Warga --}}
+    <div class=" grid grid-cols-6 md:grid-cols-3 lg:grid-cols-5 gap-5 p-6 mx-auto cursor-default" >
         @foreach ($promosi as $umkm)
-        {{-- <a href="{{url('admin/promosi/show')}}" class="group relative block overflow-hidden rounded-md"> --}}
-          <a href="{{ url('admin/promosi/show/' . $umkm->id_umkm) }}" class="group relative block overflow-hidden rounded-md">
-          <img
-            src="https://images.unsplash.com/photo-1611520189922-f7b1ba7d801e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"/>
-        
-          <div class="relative border border-gray-100 bg-white p-2">
-            <h2 class="mt-2 text-sm font-semibold text-gray-900">{{$umkm->nama_usaha}}</h2>
-            <p class="mt-0.5 text-sm text-gray-700">{{$umkm->deskripsi}}</p>
-            <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-            <p class="mt-0.5 text-xs text-gray-700">{{$umkm->alamat}}</p>
-              {{-- Filter status pengajuan --}}
-                @php
-                    $bgColor = '';
-
-                    switch($umkm->status_pengajuan) {
-                        case 'tidak aktif':
-                            $bgColor = 'bg-red-600/60';
-                            break;
-                        case 'pending':
-                            $bgColor = 'bg-yellow-600/60';
-                            break;
-                        case 'aktif':
-                            $bgColor = 'bg-teal-600/60';
-                            break;
-                        default:
-                            $bgColor = 'bg-gray-600/60'; // Warna default jika tidak cocok dengan kondisi di atas
-                            break;
-                    }
-                @endphp
-                {{-- End filter status --}}
-              <p class="mt-0.5 {{ $bgColor }} text-white px-2 rounded-full inline-block">{{ $umkm->status_pengajuan }}</p>
-
-            <form action="{{ url('admin/promosi/'.$umkm->id_umkm.'/show') }}" class="mt-2">
-              <button type="submit" class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                Lihat Detail
-              </button>
-            </form>
+          <div class="rounded-lg shadow-md flex flex-col">
+              <div class="relative">
+                  <img src="https://images.unsplash.com/photo-1611520189922-f7b1ba7d801e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                       alt="Gambar Usaha"
+                       class="w-full h-48 object-cover rounded-t-lg">
+                  <div class="mb-2 absolute inset-0 flex items-end justify-center">
+                      <form action="{{ url('admin/promosi/'.$umkm->id_umkm.'/show') }}">
+                          <button type="submit" class="bg-teal-500/70 py-2 px-4 text-sm font-normal rounded-full text-white transition duration-300 ease-in-out hover:bg-teal-600">
+                              Lihat Detail
+                          </button>
+                      </form>
+                  </div>
+              </div>
+              <div class="p-4 flex-1">
+                  <h2 class="text-lg font-semibold text-gray-900">{{ $umkm->nama_usaha }}</h2>
+                  <div class="mb-2">
+                      <p class="text-xs text-gray-600">Lokasi: {{ $umkm->alamat }}</p>
+                      <span class="text-xs text-white px-2 py-1 rounded bg-{{ $umkm->status_pengajuan === 'tidak aktif' ? 'red' : ($umkm->status_pengajuan === 'pending' ? 'yellow' : ($umkm->status_pengajuan === 'aktif' ? 'teal' : 'gray')) }}-600/60 mr-2">{{ $umkm->status_pengajuan }}</span>
+                  </div>
+              </div>
           </div>
-        </a>
-        @endforeach
-      {{-- End Usaha warga dari database --}}
-          {{-- Usaha Warga 1 --}}
-          <a href="{{url('admin/promosi/show')}}" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1611520189922-f7b1ba7d801e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Sate Maranggi Pak Ahmadi</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Cita rasa legenda sate maranggi</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-              <p class="mt-0.5 bg-yellow-600/60 text-white px-2 rounded-full inline-block">pending</p>
-              <form action="{{ url('admin/promosi/show') }}" class="mt-2">
-                <button type="submit" class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
-          {{-- Usaha Warga 2 --}}
-          <a href="#" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1611520189922-f7b1ba7d801e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Sate Maranggi Pak Ahmadi</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Cita rasa legenda sate maranggi</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-              <p class="mt-0.5 bg-yellow-600/60 text-white px-2 rounded-full inline-block">pending</p>
-              <form class="mt-2">
-                <button class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
-          {{-- Usaha Warga 3 --}}
-          <a href="#" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1514327567052-1eed4e4902c1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Burger brother</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Menjual burger aneka topping dan hot dog</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-              <p class="mt-0.5 bg-yellow-600/60 text-white px-2 rounded-full inline-block">pending</p>
-              <form class="mt-2">
-                <button class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
-          {{-- Usaha Warga 2 --}}
-          <a href="#" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1514327567052-1eed4e4902c1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Burger brother</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Menjual burger aneka topping dan hot dog</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-          
-              <form class="mt-4">
-                <button class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
-          {{-- Usaha Warga 2 --}}
-          <a href="#" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1514327567052-1eed4e4902c1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Burger brother</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Menjual burger aneka topping dan hot dog</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-          
-              <form class="mt-4">
-                <button class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
-          {{-- Usaha Warga 2 --}}
-          <a href="#" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1514327567052-1eed4e4902c1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Burger brother</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Menjual burger aneka topping dan hot dog</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-          
-              <form class="mt-4">
-                <button class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
-          {{-- Usaha Warga 2 --}}
-          <a href="#" class="group relative block overflow-hidden rounded-md">
-            <img
-              src="https://images.unsplash.com/photo-1514327567052-1eed4e4902c1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-              class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-            />
-          
-            <div class="relative border border-gray-100 bg-white p-2">
-              <h2 class="mt-2 text-sm font-semibold text-gray-900">Burger brother</h2>
-              <p class="mt-0.5 text-sm text-gray-700">Menjual burger aneka topping dan hot dog</p>
-              <p class="mt-0.5 text-xs text-gray-700">Lokasi :</p>
-              <p class="mt-0.5 text-xs text-gray-700">Sebelah gang rt.4</p>
-          
-              <form class="mt-4">
-                <button class="block w-full rounded bg-teal-400 p-2 text-sm font-normal transition hover:scale-105">
-                  Lihat Detail
-                </button>
-              </form>
-            </div>
-          </a>
+      @endforeach
         {{-- End Bagian Usaha Warga --}}
     </div>
   <!-- end content -->
--- resources/views/admin/promosi/show.blade.php
+++ resources/views/admin/promosi/show.blade.php
@@ -21,109 +21,54 @@
     </div>
     @else
     {{-- End bagian alert data --}}
-    {{-- Start Detail Informasi usaha warga --}}
-    <div class="container mx-auto mt-8 mb-5">
-        <div class="bg-teal-600 px-4 py-2 rounded-t-lg">
-            <h2 class="text-lg font-semibold text-white border-b-2 border-white pb-1">{{ $page->title }}</h2>
-        </div>
-        <div class="bg-white shadow-md rounded-b-lg overflow-hidden">
-            <div class="flex">
-                <div class="w-1/2 flex items-center justify-start">
-                    <img src="{{ $promosi->image_url }}" alt="{{ $promosi->nama_usaha }}" class="max-w-full h-auto mx-auto mt-5 mb-5 ml-5 rounded-xl" style="max-width: 400px; max-height: 300px;">
-                </div>
-                <div class="w-1/2 px-6 py-4">
-                    <h3 class="text-lg font-semibold text-gray-800 mb-2">Nama Usaha: {{ $promosi->nama_usaha }}</h3>
-                    <p class="text-gray-700 mb-2">Deskripsi Usaha: {{ $promosi->deskripsi }}</p>
-                    <p class="text-gray-700 mb-2">Lokasi: {{ $promosi->alamat }}</p>
-                    <p class="text-gray-700 mb-2">KK Pemilik Usaha: {{ $nomor_kk}}</p>
-                    {{-- <p class="text-gray-700 mb-2">Nama Warga: {{ $promosi->nama_warga }}</p> --}}
-                    {{-- <p class="text-gray-700 mb-2">NIK: {{ $promosi->nik }}</p> --}}
-                    <div class="bg-transparent flex mb-2">
-                        <p class="text-gray-700">Status :</p>
-                        {{-- Filter status_pengajuan --}}
-                        @php
-                            $bgColor = '';
-                            switch($promosi->status_pengajuan) {
-                                case 'tidak aktif':
-                                    $bgColor = 'bg-red-600/60';
-                                    break;
-                                case 'pending':
-                                    $bgColor = 'bg-yellow-600/60';
-                                    break;
-                                case 'aktif':
-                                    $bgColor = 'bg-teal-600/60';
-                                    break;
-                                default:
-                                    $bgColor = 'bg-gray-600/60'; // Warna default jika tidak cocok dengan kondisi di atas
-                                    break;
-                            }
-                        @endphp
-                        {{-- End filter status_pengajuan --}}
-                        <p class="mt-0.5 {{ $bgColor }} text-white px-2 rounded-full inline-block">{{ $promosi->status_pengajuan }}</p>
-                    </div>
-                    <div class="flex justify-start mt-4">
-                        <button class="bg-teal-400 text-teal-900 text-center hover:bg-teal-700 hover:text-white text-sm font-medium px-3 py-1 rounded-full mr-2">Terima</button>
-                        <button class="bg-red-400 text-red-900 text-center hover:bg-red-600 hover:text-white text-sm font-medium px-3 py-1 rounded-full">Tolak</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-            
-    {{-- End informasi usaha warga --}}
-    <div class="flow-root my-5 bg-white rounded-lg border border-gray-100 py-3 shadow-md">
-        <dl class="my-3 text-sm">
+    <div class="flex my-5 bg-white rounded-lg border py-3 shadow-md ">
+      <div class="w-1/2 px-4 flex items-center justify-start">
+          <img src="https://images.unsplash.com/photo-1611520189922-f7b1ba7d801e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+               alt="Gambar Usaha"
+               class="w-full h-96 rounded-md object-cover">
+      </div>
+      <dl class="my-3 text-sm">
           <div class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-            <h2 class="text-gray-700 text-xl sm:col-span-2 underline">{{ $page->title }}</h2>
+              <dt class="font-medium text-base text-gray-900">Nama Usaha</dt>
+              <dd class="text-gray-700 sm:col-span-2">{{$promosi->nama_usaha}}</dd>
           </div>
-
+  
           <div class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">Nama Usaha</dt>
-            <dd class="text-gray-700 sm:col-span-2">{{$promosi->nama_usaha}}</dd>
+              <dt class="font-medium text-base text-gray-900">Deskripsi</dt>
+              <dd class="text-gray-700 sm:col-span-2">{{$promosi->deskripsi}}</dd>
           </div>
-      
+  
           <div class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">Deskripsi</dt>
-            <dd class="text-gray-700 sm:col-span-2">{{$promosi->deskripsi}}</dd>
+              <dt class="font-medium text-base text-gray-900">Alamat Usaha</dt>
+              <dd class="text-gray-700 sm:col-span-2">{{$promosi->alamat}}</dd>
           </div>
-      
+  
           <div class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">Alamat Usaha</dt>
-            <dd class="text-gray-700 sm:col-span-2">{{$promosi->alamat}}</dd>
-          </div>
-      
-          <div class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">KK Pemilik usaha</dt>
-            <dd class="text-gray-700 sm:col-span-2">{{$nomor_kk}}</dd>
+              <dt class="font-medium text-base text-gray-900">KK Pemilik usaha</dt>
+              <dd class="text-gray-700 sm:col-span-2">{{$nomor_kk}}</dd>
           </div>
           
           <div class="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
-            <dt class="font-medium text-gray-900">Status Pengajuan</dt>
-                    {{-- Filter status_pengajuan --}}
-                    @php
-                    $bgColor = '';
-                    switch($promosi->status_pengajuan) {
-                        case 'tidak aktif':
-                            $bgColor = 'bg-red-600/60 text-red-800';
-                            break;
-                        case 'pending':
-                            $bgColor = 'bg-yellow-600/60 text-yellow-800';
-                            break;
-                        case 'aktif':
-                            $bgColor = 'bg-teal-600/60 text-teal-800';
-                            break;
-                        default:
-                            $bgColor = 'bg-gray-600/60'; // Warna default jika tidak cocok dengan kondisi di atas
-                            break;
-                    }
-                    @endphp
-                    {{-- End filter status_pengajuan --}}
-            <dd class="w-14 px-2 text-center {{ $bgColor }} sm:col-span-2 rounded-full inline-block">{{$promosi->status_pengajuan}}</dd>
-            </div>
-        </dl>
-      </div>
+              <dt class="font-medium text-base text-gray-900">Status Pengajuan</dt>
+              <span class="text-xs text-white px-2 py-1 rounded bg-{{ $promosi->status_pengajuan === 'tidak aktif' ? 'red' : ($promosi->status_pengajuan === 'pending' ? 'yellow' : ($promosi->status_pengajuan === 'aktif' ? 'teal' : 'gray')) }}-600/60 mr-2 w-14 text-center">{{$promosi->status_pengajuan}}</span>  
+                @if($promosi->status_pengajuan === 'pending')
+                    <div class="flex justify-start mt-2">
+                        <form action="{{ url('/promosi/' . $promosi->id_umkm . '/acc') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="bg-teal-500 text-teal-800 text-center hover:bg-teal-600 hover:text-white transition duration-300 ease-in-out text-sm font-medium px-3 py-1 rounded mr-2">Terima</button>
+                        </form>
+                        <form action="{{ url('/promosi/' . $promosi->id_umkm . '/tolak') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="bg-red-500 text-red-800 text-center hover:bg-red-600 hover:text-white transition duration-300 ease-in-out text-sm font-medium px-3 py-1 rounded">Tolak</button>
+                        </form>
+                    </div>
+                @endif
+          </div>
+      </dl>
+  </div>
+  
 
-      <a href="{{ url('admin/promosi') }}" class="text-white bg-teal-400 hover:bg-teal-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Kembali</a>      
+      <a href="{{ url('admin/promosi/daftar') }}" class="text-white bg-teal-400 hover:bg-teal-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Kembali</a>      
     @endif
 
     <!-- end content -->
