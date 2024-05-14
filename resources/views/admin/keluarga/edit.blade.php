37f5174f5ae674155ba870aefc4700689467e8fb
Alerakha <capupucino@gmail.com>
2024-05-14 11:26:31 +0700

    perbaikan view file edit

--- resources/views/admin/keluarga/edit.blade.php
+++ resources/views/admin/keluarga/edit.blade.php
@@ -19,54 +19,98 @@
                 </button>
             </div>
             @else
-            <form class="px-10 py-10 min-w-full bg-white max-w-3xl grid grid-cols-4 gap-x-20 gap-y-4 outline-none outline-4 outline-gray-700 rounded-xl" action="{{ url('admin/keluarga/' . $keluarga->id_keluarga) }}" method="POST">
-                <h1 class="px-5 pb-5 pt-10 mb-5 font-semibold text-center text-xl rtl:text-right text-gray-900 border-b-2 col-span-full">
-                    {{ $page->title }}
+            <form class="px-10 py-10 bg-white outline-none outline-4 outline-gray-700 rounded-xl" action="{{ url('admin/keluarga/' . $keluarga->id_keluarga) }}" method="POST">
+                <h1 class="pb-5 mb-10 font-semibold text-center text-lg rtl:text-right text-gray-900 border-b-2">
+                    {{$page->title}}
                 </h1>
                 @csrf
                 @method('POST')
-            
                 {{-- Nomor Keluarga --}}
-                <div class="col-span-4">
-                    <label for="nomor_keluarga" class="block mb-2 text-xs font-bold text-gray-900">Nomor Keluarga<span class="text-red-500">*</span></label>
-                    <input type="text" name="nomor_keluarga" id="nomor_keluarga" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Nomor Keluarga" value="{{ $keluarga->nomor_keluarga }}" required />
-                    <small id="nomor_keluarga_help" class="text-red-500 hidden">Nomor keluarga harus terdiri dari 16 digit.</small>
-                </div>
-            
-                {{-- Upload Foto KK --}}
-                <div class="col-span-4">
-                    <label for="kk_photo" class="block mb-2 text-xs font-bold text-gray-900">Foto KK<span class="text-red-500">*</span></label>
-                    <input type="file" name="foto_kk" id="kk_photo" accept="image/*" class="shadow-sm bg-teal-400 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" required />
-                </div>
-            
-                {{-- Jumlah Tanggungan --}}
-                <div class="col-span-4 sm:col-span-2">
-                    <label for="jumlah_tanggungan" class="block mb-2 text-xs font-bold text-gray-900">Jumlah Tanggungan<span class="text-red-500">*</span></label>
-                    <input type="number" name="jumlah_tanggungan" id="jumlah_tanggungan" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Jumlah Tanggungan" value="{{ $keluarga->jumlah_tanggungan }}" required />
-                </div>
-            
-                {{-- Jumlah Kendaraan --}}
-                <div class="col-span-4 sm:col-span-2">
-                    <label for="jumlah_kendaraan" class="block mb-2 text-xs font-bold text-gray-900">Jumlah Kendaraan<span class="text-red-500">*</span></label>
-                    <input type="number" name="jumlah_kendaraan" id="jumlah_kendaraan" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Jumlah Kendaraan" value="{{ $keluarga->jumlah_kendaraan }}" required />
-                </div>
-            
-                {{-- Luas Tanah --}}
-                <div class="col-span-4 sm:col-span-2">
-                    <label for="luas_tanah" class="block mb-2 text-xs font-bold text-gray-900">Luas Tanah<span class="text-red-500">*</span></label>
-                    <input type="number" step="any" name="luas_tanah" id="luas_tanah" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Luas Tanah" value="{{ $keluarga->luas_tanah }}" required />
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4">
+                        <label for="nomor_keluarga" class="block mb-2 text-xs font-bold text-gray-900">Nomor Keluarga<span class="text-red-500">*</span></label>
+                        <input type="text" name="nomor_keluarga" id="nomor_keluarga" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Nomor Keluarga" value="{{ $keluarga->nomor_keluarga }}" required />
+                        <small id="nomor_keluarga_help" class="text-red-500 hidden">Nomor keluarga harus terdiri dari 16 digit.</small>
+                    </div>
+                    <div class="relative col-span-2">
+                        <input type="file" name="foto_kk" id="foto_kk" accept="image/*" class="absolute inset-0 opacity-0 cursor-pointer w-full h-full" required />
+                        {{-- <input type="file" name="foto_kk" id="foto_kk" accept="image/*" class="shadow bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" required /> --}}
+                        <label for="foto_kk" class="text-xs font-medium cursor-pointer text-white py-2 px-4 bg-teal-500 rounded-lg hover:bg-teal-600 transition duration-300 ease-in-out">
+                            Submit Foto KK (Max ukuran 2MB)
+                        </label>
+                        <div id="uploadIndicator_kk" class="hidden mt-2">
+                            <span class="text-green-500">Gambar Terunggah</span>
+                        </div>
+                    </div>
                 </div>
                 
+                {{-- Jumlah Kendaraan --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4">
+                        <label for="jumlah_kendaraan" class="block mb-2 text-xs font-bold text-gray-900">Jumlah Kendaraan<span class="text-red-500">*</span></label>
+                        <input type="number" name="jumlah_kendaraan" id="jumlah_kendaraan" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Jumlah Kendaraan" value="{{ $keluarga->jumlah_kendaraan }}" required />
+                    </div>
+                </div>
+
+                {{-- Jumlah Tanggungan --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="jumlah_tanggungan" class="block mb-2 text-xs font-bold text-gray-900">Jumlah Tanggungan<span class="text-red-500">*</span></label>
+                        <input type="number" name="jumlah_tanggungan" id="jumlah_tanggungan" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Jumlah Tanggungan" value="{{ $keluarga->jumlah_tanggungan }}" required />
+                    </div>
+                </div>
+
+                {{-- Jumlah Orang Kerja --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="jumlah_orang_kerja" class="block mb-2 text-xs font-bold text-gray-900">Jumlah Orang Kerja<span class="text-red-500">*</span></label>
+                        <input type="number" name="jumlah_orang_kerja" id="jumlah_orang_kerja" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Jumlah Orang Kerja" value="{{ $keluarga->jumlah_orang_kerja }}" required />
+                    </div>
+                </div>
+                {{-- Luas Tanah --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="jumlah_orang_kerja" class="block mb-2 text-xs font-bold text-gray-900">Luas Tanah<span class="text-red-500">*</span></label>
+                        <input type="number" name="luas_tanah" id="luas_tanah" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Luas Tanah" value="{{ $keluarga->luas_tanah }}" required />
+                    </div>
+                </div>
+
                 {{-- Alamat --}}
-                <div class="col-span-4 sm:col-span-2">
-                    <label for="alamat" class="block mb-2 text-xs font-bold text-gray-900">Alamat<span class="text-red-500">*</span></label>
-                    <input type="text" step="any" name="alamat" id="alamat" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Alamat" value="{{ $keluarga->alamat }}" required />
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4">
+                        <label for="alamat" class="block mb-2 text-xs font-bold text-gray-900">Alamat KK<span class="text-red-500">*</span></label>
+                        <input type="text" name="alamat" id="alamat" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Alamat" value="{{ $keluarga->alamat }}" required />
+                    </div>
+                </div>
+
+                {{-- Kelurahan, Kecamatan, Kota --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="kelurahan" class="block mb-2 text-xs font-bold text-gray-900">Kelurahan<span class="text-red-500">*</span></label>
+                        <input type="text" name="kelurahan" id="kelurahan" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Kelurahan" value="{{ $keluarga->kelurahan }}" required />
+                    </div>
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="kecamatan" class="block mb-2 text-xs font-bold text-gray-900">Kecamatan<span class="text-red-500">*</span></label>
+                        <input type="text" name="kecamatan" id="kecamatan" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Kecamatan" value="{{ $keluarga->kecamatan }}" required />
+                    </div>
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="kota" class="block mb-2 text-xs font-bold text-gray-900">Kota<span class="text-red-500">*</span></label>
+                        <input type="text" name="kota" id="kota" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Kota" value="{{ $keluarga->kota }}" required />
+                    </div>
+                    <div class="col-span-4 sm:col-span-2">
+                        <label for="rt" class="block mb-2 text-xs font-bold text-gray-900">RT<span class="text-red-500">*</span></label>
+                        <input type="number" name="rt" id="rt" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan RT" value="{{ $keluarga->rt }}" required />
+                    </div>
+                    <div class="col-span-4 sm:col-span-2 hidden">
+                        <label for="rw" class="block mb-2 text-xs font-bold text-gray-900">RW<span class="text-red-500">*</span></label>
+                        <input type="hidden" value="1" name="rw" id="rw" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan RW" required />
+                    </div>
                 </div>
             
                 {{-- Button --}}
-                <div class="flex justify-between grid-cols-2">
-                    <a href="{{ url('admin/keluarga') }}" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-lg text-xs w-full sm:w-auto px-5 py-2.5 text-center mr-2">Batal</a>
-                    <button type="submit" class="text-white bg-teal-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs w-full sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
+                <div class="flex justify-start col-span-1">
+                    <a href="{{ url('admin/keluarga') }}" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center mr-2">Batal</a>
+                    <button type="submit" class="text-white bg-teal-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
                 </div>
             </form>
             
@@ -88,5 +132,32 @@
         } else {
             errorMessage.classList.remove("hidden");
         }
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const nomorKeluargaInput = document.getElementById('nomor_keluarga');
+        const nomorKeluargaHelp = document.getElementById('nomor_keluarga_help');
+
+        // Validasi nomor keluarga harus 16 digit
+        nomorKeluargaInput.addEventListener('input', function() {
+            if (nomorKeluargaInput.value.length !== 16) {
+                nomorKeluargaHelp.classList.remove('hidden');
+            } else {
+                nomorKeluargaHelp.classList.add('hidden');
+            }
+        });
+
+        // Upload indicator
+        const kkPhotoInput = document.getElementById('foto_kk');
+        const uploadIndicatorKK = document.getElementById('uploadIndicator_kk');
+
+        kkPhotoInput.addEventListener('change', function() {
+            if (kkPhotoInput.files.length > 0) {
+                uploadIndicatorKK.classList.remove('hidden');
+            } else {
+                uploadIndicatorKK.classList.add('hidden');
+            }
+        });
+
     });
 </script>
