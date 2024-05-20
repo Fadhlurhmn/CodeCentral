3610d5cd5a1ac1b13c30bfe98fd0e86bdfd1f01b
mkemaln <kemalmuhammad2509@gmail.com>
2024-05-20 07:06:58 +0700

    Update view for index and change  routing for pengumuman

--- resources/views/admin/pengumuman/index.blade.php
+++ resources/views/admin/pengumuman/index.blade.php
@@ -1,69 +1,60 @@
-@csrf
-<div class="container h-full bg-slate-100">
-  {{-- start breadcrumb --}}
-  <h1 class="py-5 ml-5 text-3xl text-gray-900">{{$breadcrumb->title}}</h1>
-  {{-- end breadcrumb --}}
-  <div class="p-5 text-xl font-semibold text-left rtl:text-right text-gray-900 bg-white outline-none outline-4 outline-teal-500 rounded-t-3xl">
-    {{-- box table title   --}}
-    <p class="my-5 text-xl font-normal text-gray-600 ">{{$page->title}}</p>
-      
-      {{-- tombol create pengumuman --}}
-      <a class="p-2 font-normal text-center text-sm bg-teal-300 hover:bg-teal-500 text-teal-700 hover:text-gray-700 rounded-lg" href="{{url('pengumuman/create')}}">Tambah Pengumuman</a>
-      
-      <div class="relatives mt-5 h-80 overflow-x-auto p-5 shadow">
-        {{-- start datatable --}}
-          <table id="table_pengumuman" class="table-auto text-center border w-full min-w-max cursor-default">
-              <thead class="bg-teal-400">
-                  <tr>
-                      <th class="p-3 text-lg font-normal justify-between tracking-wide border-r">No</th>
-                      <th class="p-3 text-lg font-normal justify-between tracking-wide border-r">Judul</th>
-                      <th class="p-3 text-lg font-normal justify-between tracking-wide border-r">Tanggal publish</th>
-                      <th class="p-3 text-lg font-normal justify-between tracking-wide border-r bg-">Aksi</th>
-                  </tr>
-              </thead>
-          </table>
-        {{-- end datatable --}}
-      </div>
-  </div>
-</div>
+@include('layout.start')
 
-@push('js')
-<script>
-  // datatable config
-  $(document).ready(function() {
-      var table = $('#table_pengumuman').DataTable({
-          serverSide: true,
-          ajax: {
-              "url": "{{ url('pengumuman/list') }}",
-              "dataType": "json",
-              "type": "POST",
-          },
-          columns: [{
-                  data: "DT_RowIndex",
-                  className: "text-center text-sm",
-                  orderable: false,
-                  searchable: false
-              },
-              {
-                  data: "judul_pengumuman",
-                  className: "text-sm",
-                  orderable: true,
-                  searchable: true
-              },
-              {
-                  data: "created_at",
-                  className: "text-sm",
-                  orderable: true,
-                  searchable: true
-              },
-              {
-                  data: "aksi",
-                  className: "flex text-sm",
-                  orderable: false,
-                  searchable: false
-              },
-          ]
-      });
-  });
-</script>
-@endpush
+
+@include('layout.a_navbar')
+
+
+<!-- strat wrapper -->
+<div class="h-screen flex flex-row flex-wrap ">
+  
+  @include('layout.a_sidebar')
+
+  <!-- start content -->
+  <div class="bg-white flex-1 md:mt-16 cursor-default"> 
+    <div class="container p-5 mb-5 bg-white border-t-4 border-teal-500 text-lg flex flex-col items-start">
+      <h1 class="py-5 text-3xl text-gray-900 font-bold">{{$breadcrumb->title}}</h1>
+      <p class="mb-3 text-xl">{{$page->title}}</p>
+      <div class="flex justify-between w-full">
+        <a class="p-2 text-sm font-normal text-center shadow-md bg-teal-300 hover:bg-teal-500 text-teal-700 hover:text-gray-700 transition duration-300 ease-in-out rounded-lg" href="{{url('admin/pengumumman/create')}}">Tambah Pengumuman</a>
+        {{-- Search form --}}
+        <form action="{{ url()->current() }}" method="GET" class="text-sm font-medium">
+          <input type="text" name="query" value="{{ request('query') }}" placeholder="Cari pengumuman" class="px-4 py-2 border border-gray-300 rounded-md">
+          <button type="submit" class="px-4 py-2 bg-teal-400 text-teal-900 rounded-md ml-2">Cari</button>
+        </form>
+      </div>
+    </div>
+      {{-- Bagian pengumuman Usaha Warga --}}
+      <div class="h-fit grid grid-cols-1 gap-5 p-6 mx-auto bg-white/50 border-t-4 border-teal-400 cursor-default">
+        @foreach ($pengumuman as $pengumumans)
+            <div class="rounded-lg shadow-md flex flex-row border-b-2 border-teal-500">
+                <div class="p-4 flex-1">
+                    <h2 class="text-lg font-semibold text-gray-900">{{ $pengumumans->judul_pengumuman }}</h2>
+                    <p class="text-xs text-gray-600">Penulis: {{ $pengumumans->id_user }}</p>
+                    <div class="mb-2">
+                        <p class="text-xs text-gray-600">Tanggal penulisan: {{ $pengumumans->created_at }}</p>
+                    </div>
+                </div>
+                <div class="mr-4 flex flex-row items-center justify-center">
+                    <form action="{{ url('admin/pengumuman/'.$pengumumans->id_pengumuman.'/show') }}">
+                        <button type="submit" class="bg-teal-500/70 py-2 px-4 mr-2 text-sm font-normal rounded-full text-white transition duration-300 ease-in-out hover:bg-teal-600">
+                            Preview
+                        </button>
+                    </form>
+                    <form action="{{ url('admin/pengumuman/'.$pengumumans->id_pengumuman.'/edit') }}">
+                        <button type="submit" class="bg-yellow-200 py-2 px-4 text-sm font-normal rounded-full text-yellow-500 transition duration-300 ease-in-out hover:bg-yellow-300">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        @endforeach
+     </div>
+     {{-- End Bagian pengumuman Usaha Warga --}}
+  
+  
+  
+  <!-- end content -->
+</div>
+</div>
+<!-- end wrapper -->
+@include('layout.end')
