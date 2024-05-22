5493c74eebba9d2352719963fe1c54fc5873faae
Alerakha <capupucino@gmail.com>
2024-05-22 17:41:23 +0700

    Tampilan awal jadwal

--- resources/views/admin/jadwal/index.blade.php
+++ resources/views/admin/jadwal/index.blade.php
@@ -1,26 +1,153 @@
-@include('layout.start')
-
-
-@include('layout.a_navbar')
-
-
-<!-- strat wrapper -->
-<div class="h-screen flex flex-row flex-wrap">
+<div class="container h-full bg-white cursor-default">
+  {{-- <h1 class="py-5 ml-5 text-3xl text-gray-900 font-bold">{{$breadcrumb->title}}</h1> --}}
   
-  @include('layout.a_sidebar')
+  <div class="p-5 text-sm font-normal rtl:text-right text-gray-900 bg-white border-t-2 border-teal-500">
+    <h1 class="pb-5 my-2 text-2xl font-extrabold text-gray-600">{{$page->title}}</h1>
 
-  <!-- start content -->
-  <div class="bg-gray-100 flex-1 p-6 md:mt-16"> 
+    {{-- Data dummy --}}
+    @php
+      $jadwal_kebersihan = [
+          'hari' => 'Senin',
+          'waktu' => '08:00 - 12:00'
+      ];
+      $jadwal_keamanan = [
+          'hari' => ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+          'waktu' => ['Pagi', 'Sore', 'Malam'],
+          'nama' => ['Budi', 'Adi', 'Dedi'],
+          'telepon' => ['08123456789', '082122222222', '08211111111'],
+      ];
+    @endphp
 
-    
-    {{-- Place your code here --}}
-    <div class="text-lg">Ini adalah Halaman Jadwal Petugas (MEDIUM)</div>
-        
+    <!-- Tabel Jadwal Keamanan -->
+    {{-- Button --}}
+    <div class="mb-5 text-xs">
+      <a class="p-2 font-normal text-center shadow-md hover:shadow-teal-300 bg-teal-300 hover:bg-teal-400 text-teal-700 hover:text-teal-700 transition duration-300 ease-in-out rounded-lg" href="{{ url('admin/jadwal/update_keamanan') }}">Ubah Jadwal Keamanan</a>
+    </div>
+    {{-- Tabel --}}
+    <div class="h-auto p-2 mb-10 border-2 border-teal-400 rounded-lg">
+      <h2 class="pb-5 my-2 text-xl font-bold text-gray-600">Jadwal Keamanan</h2>
+      <table id="table_keamanan" class="w-full min-w-max cursor-default border-collapse">
+        <thead class="bg-teal-400 text-center">
+          <tr>
+            <th class="p-3 text-sm font-normal border border-teal-300">Shift</th>
+            @foreach ($jadwal_keamanan['hari'] as $hari)
+              <th class="p-3 text-sm font-normal border border-teal-300">{{ $hari }}</th>
+            @endforeach
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($jadwal_keamanan['waktu'] as $index => $waktu)
+            <tr>
+              <td class="p-3 text-sm text-center bg-teal-300 border border-teal-300">{{ $waktu }}</td>
+              @foreach ($jadwal_keamanan['hari'] as $hariIndex => $hari)
+                <td class="p-3 text-sm text-center border border-teal-300">
+                  {{ $jadwal_keamanan['nama'][$index % count($jadwal_keamanan['nama'])] }}<br>
+                  <span class="text-xs text-gray-500">({{ $jadwal_keamanan['telepon'][$index % count($jadwal_keamanan['telepon'])] }})</span>
+                </td>
+              @endforeach
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+
+
+    <!-- Tabel Jadwal Kebersihan -->
+    {{-- Button --}}
+    <div class="mb-5 text-xs">
+        <a class="p-2 font-normal text-center shadow-md hover:shadow-teal-300 bg-teal-300 hover:bg-teal-400 text-teal-700 hover:text-teal-700 transition duration-300 ease-in-out rounded-lg" href="{{ url('admin/jadwal/update_kebersihan') }}">Ubah Jadwal Kebersihan</a>
+    </div>
+    {{-- Tabel --}}
+    <div class="h-auto p-2 mb-10 border-2 border-teal-400 rounded-lg">
+      <h2 class="pb-5 my-2 text-xl font-bold text-gray-600">Jadwal Kebersihan</h2>
+      <table id="table_kebersihan" class="w-full min-w-max cursor-default border-collapse">
+        <thead class="bg-teal-400 text-center">
+          <tr>
+            <th class="p-3 text-sm font-normal border border-teal-300">Hari</th>
+            <th class="p-3 text-sm font-normal border border-teal-300">Waktu</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="p-3 text-sm text-center border border-teal-300">{{ $jadwal_kebersihan['hari'] }}</td>
+            <td class="p-3 text-sm text-center border border-teal-300">{{ $jadwal_kebersihan['waktu'] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
   </div>
-  <!-- end content -->
-
 </div>
-<!-- end wrapper -->
 
-@include('layout.end')
+
+@push('js')
+{{-- <script>
+  $(document).ready(function() {
+      var table = $('#table_penduduk').DataTable({
+          serverSide: true,
+          ajax: {
+              "url": "{{ url('admin/penduduk/list') }}",
+              "dataType": "json",
+              "type": "POST",
+          },
+          columns: [
+              { data: "DT_RowIndex",
+               className: "text-xs border-b border-gray-500/40",
+                orderable: false, 
+                searchable: false },
+              { data: "nik",
+               className: "text-xs border-b border-gray-500/40",
+                orderable: true,
+               searchable: true },
+              { data: "nama",
+               className: "text-xs border-b border-gray-500/40", 
+               orderable: true, 
+               searchable: true },
+              { data: "alamat_domisili",
+               className: "text-xs border-b border-gray-500/40", 
+               orderable: true },
+              { data: "rt",
+               className: "text-xs border-b border-gray-500/40", 
+               orderable: true, 
+               searchable: false },
+              { 
+                  data: "status_data",
+                  className: "text-xs border-b border-gray-500/40 text-center",
+                  orderable: true,
+                  searchable: true,
+                  render: function(data, type, row) {
+                      if (data === 'Aktif') {
+                          return '<div class="rounded-full bg-emerald-500/60 text-emerald-800 py-1 px-2">' + data + '</div>';
+                      } else {
+                          return '<div class="rounded-full bg-red-500/60 text-red-900 py-1 px-2">' + data + '</div>';
+                      }
+                  }
+              },
+              { data: "status_penduduk",
+               className: "text-xs border-b border-gray-500/40 text-center", 
+               orderable: true, 
+               searchable: true },
+              { data: "aksi",
+               className: "flex text-xs border-b border-gray-500/40", 
+               orderable: false, 
+               searchable: false },
+          ]
+      });
+
+      $('#rt').on('change', function() {
+          var selectedRt = $(this).val();
+          if (selectedRt === 'all') {
+              // Jika dipilih "Semua", atur URL tanpa parameter rt
+              table.ajax.url("{{ url('admin/penduduk/list') }}").load();
+          } else {
+              // Jika dipilih nilai lain, atur URL dengan parameter rt
+              table.ajax.url("{{ url('admin/penduduk/list') }}?rt=" + selectedRt).load();
+          }
+      });
+  });
+</script> --}}
+@endpush
+{{-- @stack('js') --}}
+<script>
+  $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
+</script>
---
+++ resources/views/admin/jadwal/update_kebersihan.blade.php
@@ -0,0 +1,55 @@
+@include('layout.start')
+
+@include('layout.a_navbar')
+
+<div class="h-screen flex flex-row flex-wrap">
+    @include('layout.a_sidebar')
+    <div class="flex-grow bg-white">
+        <div class="flex flex-col">
+            <h1 class="py-5 ml-5 text-2xl font-bold">{{ $page->title }}</h1>
+        </div>
+        <div class="w-full h-fit min-w-max p-5">
+            <form id="form" class="px-10 py-10 bg-white outline-none outline-4 outline-gray-700 rounded-xl" action="{{ url('admin/jadwal/kebersihan/create') }}" method="POST">
+                <h1 class="pb-5 mb-10 font-semibold text-center text-lg rtl:text-right text-gray-900 border-b-2">
+                    Isi Jadwal Kebersihan
+                </h1>
+                @csrf
+
+                {{-- Hari --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4">
+                        <label for="hari" class="block mb-2 text-xs font-bold text-gray-900">Hari<span class="text-red-500">*</span></label>
+                        <select name="hari" id="hari" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" required>
+                            <option value="Senin">Senin</option>
+                            <option value="Selasa">Selasa</option>
+                            <option value="Rabu">Rabu</option>
+                            <option value="Kamis">Kamis</option>
+                            <option value="Jumat">Jumat</option>
+                            <option value="Sabtu">Sabtu</option>
+                            <option value="Minggu">Minggu</option>
+                        </select>
+                    </div>
+                </div>
+                
+                {{-- Waktu --}}
+                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
+                    <div class="col-span-4">
+                        <label for="waktu" class="block mb-2 text-xs font-bold text-gray-900">Waktu<span class="text-red-500">*</span></label>
+                        <select name="waktu" id="waktu" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" required>
+                            <option value="08:00 - 12:00">08:00 - 12:00</option>
+                            <option value="12:00 - 16:00">12:00 - 16:00</option>
+                        </select>
+                    </div>
+                </div>
+
+                {{-- Submit Button --}}
+                <div class="flex col-span-1">
+                    <a href="{{ url('admin/jadwal') }}" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center mr-2">Batal</a>
+                    <button type="submit" class="text-white bg-teal-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+@include('layout.end')
