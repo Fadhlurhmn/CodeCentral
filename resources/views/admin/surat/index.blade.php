f5ce92c6bcf86c9b2be7ad0b86e16a2a50b2a5f2
Alerakha <capupucino@gmail.com>
2024-05-22 09:52:59 +0700

    View index surat

--- resources/views/admin/surat/index.blade.php
+++ resources/views/admin/surat/index.blade.php
@@ -30,8 +30,8 @@
   @if (session('success'))
         <div class="col-span-4">
             <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-                <strong class="font-bold">Sukses, Surat Berhasil Ditambahkan!</strong>
-                <span class="block sm:inline">{{ session('success') }}</span>
+                {{-- <strong class="font-bold">Sukses, Surat Berhasil Ditambahkan!</strong> --}}
+                <span class="block font-bold sm:inline">{{ session('success') }} !</span>
             </div>
         </div>
   @endif
