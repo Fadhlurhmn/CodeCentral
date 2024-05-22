38006499aa81b48a5150378dbaf92b7ef17d2938
Alerakha <capupucino@gmail.com>
2024-05-22 22:11:48 +0700

    halaman update keamanan

--- resources/views/admin/jadwal/update_keamanan.blade.php
+++ resources/views/admin/jadwal/update_keamanan.blade.php
@@ -1,69 +1,69 @@
 @include('layout.start')
 
+
 @include('layout.a_navbar')
 
+
+<!-- strat wrapper -->
 <div class="h-screen flex flex-row flex-wrap">
-    @include('layout.a_sidebar')
-    <div class="flex-grow bg-white">
-        <div class="flex flex-col">
-            <h1 class="py-5 ml-5 text-2xl font-bold">{{ $page->title }}</h1>
-        </div>
-        <div class="w-full h-fit min-w-max p-5">
-            <form id="form" class="px-10 py-10 bg-white outline-none outline-4 outline-gray-700 rounded-xl" action="{{ url('admin/jadwal/keamanan/create') }}" method="POST">
-                <h1 class="pb-5 mb-10 font-semibold text-center text-lg rtl:text-right text-gray-900 border-b-2">
-                    Isi Jadwal Keamanan
-                </h1>
-                @csrf
+  
+  @include('layout.a_sidebar')
 
-                {{-- Hari --}}
-                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
-                    <div class="col-span-4">
-                        <label for="hari" class="block mb-2 text-xs font-bold text-gray-900">Hari<span class="text-red-500">*</span></label>
-                        <select name="hari" id="hari" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" required>
-                            <option value="Senin">Senin</option>
-                            <option value="Selasa">Selasa</option>
-                            <option value="Rabu">Rabu</option>
-                            <option value="Kamis">Kamis</option>
-                            <option value="Jumat">Jumat</option>
-                            <option value="Sabtu">Sabtu</option>
-                            <option value="Minggu">Minggu</option>
-                        </select>
-                    </div>
-                </div>
-                
-                {{-- Waktu --}}
-                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
-                    <div class="col-span-4">
-                        <label for="waktu" class="block mb-2 text-xs font-bold text-gray-900">Waktu<span class="text-red-500">*</span></label>
-                        <select name="waktu" id="waktu" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" required>
-                            <option value="Pagi">Pagi</option>
-                            <option value="Sore">Sore</option>
-                            <option value="Malam">Malam</option>
-                        </select>
-                    </div>
-                </div>
+  <!-- start content -->
+  <div class="bg-white flex-1 md:mt-16"> 
 
-                {{-- Nama --}}
-                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
-                    <div class="col-span-4">
-                        <label for="nama" class="block mb-2 text-xs font-bold text-gray-900">Nama<span class="text-red-500">*</span></label>
-                        <input type="text" name="nama" id="nama" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Nama" required />
-                    </div>
-                </div>
+    
+    {{-- Place your code here --}}
+    <div class="flex-col flex-grow">
 
-                {{-- Telepon --}}
-                <div class="grid grid-cols-4 gap-x-20 gap-y-2 mb-5">
-                    <div class="col-span-4">
-                        <label for="telepon" class="block mb-2 text-xs font-bold text-gray-900">Telepon<span class="text-red-500">*</span></label>
-                        <input type="number" name="telepon" id="telepon" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="Masukkan Telepon" required />
-                    </div>
-                </div>
+{{--  --}}
+<div class="container mx-auto p-4">
+    <h2 class="text-2xl font-bold mb-4">{{ $breadcrumb->title }}</h2>
 
-                {{-- Submit Button --}}
-                <div class="flex col-span-1">
-                    <a href="{{ url('admin/jadwal') }}" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center mr-2">Batal</a>
-                    <button type="submit" class="text-white bg-teal-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
-                </div>
-            </form>
-        </div>
-   
+    <!-- Tabel Jadwal Keamanan -->
+      {{-- Tabel --}}
+      <div class="h-auto p-2 mb-10 border-2 border-teal-400 rounded-lg">
+        <h2 class="pb-5 my-2 text-xl font-bold text-gray-600">Jadwal Keamanan</h2>
+        <table id="table_keamanan" class="w-full min-w-max cursor-default border-collapse">
+          <thead class="bg-teal-400 text-center">
+            <tr>
+              <th class="p-3 text-sm font-normal border border-teal-300">Shift</th>
+              @foreach ($jadwal_keamanan->hari as $hari)
+                <th class="p-3 text-sm font-normal border border-teal-300">{{ $hari }}</th>
+              @endforeach
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($jadwal_keamanan->waktu as $index => $waktu)
+              <tr>
+                <td class="p-3 text-sm text-center bg-teal-300 border border-teal-300">{{ $waktu }}</td>
+                @foreach ($jadwal_keamanan->hari as $hariIndex => $hari)
+                  <td class="p-3 text-sm text-center border border-teal-300 cursor-pointer"  contenteditable="true">
+                    {{-- @foreach($jadwal_keamanan->waktu as $index2 => $waktu) --}}
+                    {{ $jadwal_keamanan->nama[$hariIndex][$index] }}<br>
+                    {{-- @endforeach --}}
+                    <span class="text-xs text-gray-500">({{ $jadwal_keamanan->telepon[$index] }})</span>
+                  </td>
+                @endforeach
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+      {{-- Submit Button --}}
+    <div class="flex">
+        <a href="{{ url('admin/jadwal') }}" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-400 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center mr-2">Batal</a>
+        <button type="submit" class="text-white bg-teal-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs sm:w-auto px-5 py-2.5 text-center ">Simpan</button>
+    </div>
+</div>
+{{--  --}}
+</div>
+</div>
+<!-- end content -->
+
+</div>
+<!-- end wrapper -->
+
+@include('layout.end')
+
+
