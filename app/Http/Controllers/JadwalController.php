bdd79b83c31b12d21f90a228b8dd3b41055090ef
Fadhlurhmn <fadhlu.campus22@gmail.com>
2024-05-22 18:19:03 +0700

    tambah hari minggu

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -20,9 +20,9 @@ class JadwalController extends Controller
 
         // Inisialisasi jadwal keamanan sebagai objek dengan nama sebagai array dua dimensi
         $this->jadwal_keamanan = (object)[
-            'hari' => ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+            'hari' => ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'],
             'waktu' => ['Pagi', 'Sore', 'Malam'],
-            'nama' => [['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi']],
+            'nama' => [['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi'], ['Budi', 'Adi', 'Dedi']],
             'telepon' => ['08123456789', '082122222222', '08211111111'],
         ];
     }
