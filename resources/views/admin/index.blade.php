708bc0efa318087ab7e51739facb9924353d648e
AlichiaPutri <alichiaputri21@gmail.com>
2024-05-28 23:50:23 +0700

    View dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,13 +9,83 @@
   
   @include('layout.a_sidebar')
 
-  <!-- strat content -->
+  <!-- start content -->
   <div class="bg-gray-100 flex-1 p-6 md:mt-16"> 
 
     
     <div class="text-lg">Ini adalah Halaman Dashboard (HIGH)</div>
         
 
+    <div class="grid grid-cols-4 gap-6 xl:grid-cols-1">
+
+    <!-- start dashboard -->
+    <div class="report-card">
+        <div class="card">
+            <div class="card-body flex flex-col">
+                
+                <!-- top -->
+                <div class="flex flex-row justify-between items-center">
+                    <i class="fas fa-coins text-s mr-2"></i>
+                </div>
+                <!-- end top -->
+
+                <!-- bottom -->
+                <div class="mt-8">
+                    <h1 class="h5 num-4"></h1>
+                    <p>Rekap Kas</p>
+                </div>                
+                <!-- end bottom -->
+    
+            </div>
+        </div>
+        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
+    </div>
+
+    <div class="report-card">
+        <div class="card">
+            <div class="card-body flex flex-col">
+                
+                <!-- top -->
+                <div class="flex flex-row justify-between items-center">
+                    <i class="fas fa-users text-md mr-2"></i>
+                </div>
+                <!-- end top -->
+
+                <!-- bottom -->
+                <div class="mt-8">
+                    <h1 class="h5 num-4"></h1>
+                    <p>Rekap Warga</p>
+                </div>                
+                <!-- end bottom -->
+    
+            </div>
+        </div>
+        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
+    </div>
+
+    <div class="report-card">
+        <div class="card">
+            <div class="card-body flex flex-col">
+                
+                <!-- top -->
+                <div class="flex flex-row justify-between items-center">
+                    <i class="fas fa-envelope text-md mr-2"></i>
+                </div>
+                <!-- end top -->
+
+                <!-- bottom -->
+                <div class="mt-8">
+                    <h1 class="h5 num-4"></h1>
+                    <p>Rekap Surat</p>
+                </div>                
+                <!-- end bottom -->
+    
+            </div>
+        </div>
+        <div class="footer bg-white p-1 mx-4 border border-t-0 rounded rounded-t-none"></div>
+    </div>
+
+  </div>
   </div>
   <!-- end content -->
 
