5238bc3f14f3e8a9d5ef1587120ca5fba81e5704
AlichiaPutri <alichiaputri21@gmail.com>
2024-06-06 10:20:29 +0700

    update view dashboard admin

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -16,7 +16,7 @@
     <div class="text-lg">Ini adalah Halaman Dashboard (HIGH)</div>
         
 
-    <div class="grid grid-cols-4 gap-6 xl:grid-cols-1">
+    <div class="grid grid-cols-3 gap-6 xl:grid-cols-1">
 
     <!-- start dashboard -->
     <div class="report-card">
@@ -69,7 +69,7 @@
                 
                 <!-- top -->
                 <div class="flex flex-row justify-between items-center">
-                    <i class="fa-solid fa-flag text-md mr-2"></i>
+                    <i class="fas fa-flag text-md mr-2"></i>
         
                 </div>
                 <!-- end top -->
@@ -87,90 +87,13 @@
     </div>
     </div>
 
-    <div class="container mx-auto mt-10">
-        <table id="table_penduduk" class="table-auto w-full min-w-max cursor-default mx-auto border-collapse border border-gray-300">
-          <thead class="bg-teal-400 px-10 text-center justify-between">
-            <tr>
-              <th class="border border-gray-300 px-4 py-2">No</th>
-              <th class="border border-gray-300 px-4 py-2">NIK</th>
-              <th class="border border-gray-300 px-4 py-2">Nama</th>
-              <th class="border border-gray-300 px-4 py-2">Alamat Domisili</th>
-              <th class="border border-gray-300 px-4 py-2">Rt</th>
-              <th class="border border-gray-300 px-4 py-2">Status Data</th>
-              <th class="border border-gray-300 px-4 py-2">Status Penduduk</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-                <td class="border border-gray-300 px-4 py-2">1</td>
-                <td class="border border-gray-300 px-4 py-2">123456789</td>
-                <td class="border border-gray-300 px-4 py-2">Lina</td>
-                <td class="border border-gray-300 px-4 py-2">Jl. Remujung 12</td>
-                <td class="border border-gray-300 px-4 py-2">01</td>
-                <td class="border border-gray-300 px-4 py-2">Aktif</td>
-                <td class="border border-gray-300 px-4 py-2">Tetap</td>
-            </tr>
-              <tr>
-                <td class="border border-gray-300 px-4 py-2">2</td>
-                <td class="border border-gray-300 px-4 py-2">987654321</td>
-                <td class="border border-gray-300 px-4 py-2">Amanda</td>
-                <td class="border border-gray-300 px-4 py-2">Jl. Pisang Kipas</td>
-                <td class="border border-gray-300 px-4 py-2">02</td>
-                <td class="border border-gray-300 px-4 py-2">Tidak Aktif</td>
-                <td class="border border-gray-300 px-4 py-2">Sementara</td>
-              </tr>
 
-              <tr>
-                <td class="border border-gray-300 px-4 py-2">3</td>
-                <td class="border border-gray-300 px-4 py-2">987654321</td>
-                <td class="border border-gray-300 px-4 py-2">Amanda</td>
-                <td class="border border-gray-300 px-4 py-2">Jl. Pisang Kipas</td>
-                <td class="border border-gray-300 px-4 py-2">02</td>
-                <td class="border border-gray-300 px-4 py-2">Tidak Aktif</td>
-                <td class="border border-gray-300 px-4 py-2">Sementara</td>
-              </tr>
-
-              <tr>
-                <td class="border border-gray-300 px-4 py-2">4</td>
-                <td class="border border-gray-300 px-4 py-2">987654321</td>
-                <td class="border border-gray-300 px-4 py-2">Amanda</td>
-                <td class="border border-gray-300 px-4 py-2">Jl. Pisang Kipas</td>
-                <td class="border border-gray-300 px-4 py-2">02</td>
-                <td class="border border-gray-300 px-4 py-2">Tidak Aktif</td>
-                <td class="border border-gray-300 px-4 py-2">Sementara</td>
-              </tr>
-
-              <tr>
-                <td class="border border-gray-300 px-4 py-2">5</td>
-                <td class="border border-gray-300 px-4 py-2">987654321</td>
-                <td class="border border-gray-300 px-4 py-2">Amanda</td>
-                <td class="border border-gray-300 px-4 py-2">Jl. Pisang Kipas</td>
-                <td class="border border-gray-300 px-4 py-2">02</td>
-                <td class="border border-gray-300 px-4 py-2">Tidak Aktif</td>
-                <td class="border border-gray-300 px-4 py-2">Sementara</td>
-              </tr>
-
-            <!-- Table rows go here -->
-          </tbody>
-        </table>
-    </div> 
   <!-- Charts -->
+  <div class="grid grid-cols-2 gap-6 xl:grid-cols-1">
   <div class="row">
     <div class="mt-5">
-    <div class="col-lg-8 col-md-12 col-sm-12">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Penduduk Masuk dan Keluar</h5>
-                <div class="chart-container">
-                    <canvas id="barChart"></canvas>
-                </div>
-            </div>
-        </div>
-    </div>
-<div>
-    <div class="mt-5">
     <div class="bg-white p-4 rounded-lg shadow-md">
-                    <h2 class="text-lg mb-2">Jenis Kelamin</h2>
+                    <h2 class="text-lg mb-2">Statistik Warga Tiap RT</h2>
                     <div class="chart-container pie-chart-container">
                         <canvas id="pieChart"></canvas>
                     </div>
@@ -182,53 +105,22 @@
     <!-- Include Chart.js via CDN -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        // Line Chart
-        var ctx = document.getElementById('barChart').getContext('2d');
-        var barChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
-                datasets: [
-                    {
-                        label: 'Penduduk Masuk',
-                        data: [2, 0, 1, 26, 0, 0, 0, 28, 0, 30, 0, 0],
-                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                        borderColor: 'rgba(54, 162, 235, 1)',
-                        borderWidth: 1
-                    },
-                    {
-                        label: 'Penduduk Keluar',
-                        data: [0, 1, 0, 15, 0, 0, 0, 0, 0, 10, 0, 0],
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
 
         // Pie Chart
         const pieCtx = document.getElementById('pieChart').getContext('2d');
         const pieChart = new Chart(pieCtx, {
             type: 'pie',
             data: {
-                labels: ['Perempuan', 'Laki-Laki'],
+                labels: ['RT 1', 'RT 2', 'RT 3', 'RT 4', 'RT 5'],
                 datasets: [{
-                    label: 'Jenis Kelamin',
-                    data: [30, 15],
+                    label: 'Jumlah Warga',
+                    data: [17, 16,16,18,16],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)'
+                        'rgba(46, 139, 87)',
+                        'rgba(27, 128, 1)',
+                        'rgba(107, 142, 35)',
+                        'rgba(60, 179, 113)',
+                        'rgba(144, 238, 144)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -245,7 +137,11 @@
                 maintainAspectRatio: false
             }
         });
+        
+        
     </script>
+
+    
     
 
   
