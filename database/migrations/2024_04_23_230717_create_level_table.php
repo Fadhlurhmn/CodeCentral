51dd46738bdfefe81d3f3c9e96bf3e2893c37534
Fadhlurhmn <fadhlu.campus22@gmail.com>
2024-06-07 09:57:48 +0700

    perbaikan user seeder dan level seeder

--- database/migrations/2024_04_23_230717_create_level_table.php
+++ database/migrations/2024_04_23_230717_create_level_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('level', function (Blueprint $table) {
             $table->id('id_level');
-            $table->string('kode_level', 5)->unique();
             $table->string('nama_level', 20);
             $table->timestamps();
         });
